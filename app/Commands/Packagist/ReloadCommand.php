2369aae1ba2973accf639c1848fbcbbd3efd23fd
kawax <kawaxbiz@gmail.com>
2019-01-07 01:53:14 +0900

    reload

---
+++ app/Commands/Packagist/ReloadCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Commands\Packagist;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+use Illuminate\Support\Facades\Artisan;
+
+class ReloadCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'packagist:reload';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Artisan::call('packagist:root');
+        Artisan::call('packagist:get');
+        Artisan::call('packagist:index');
+        Artisan::call('packagist:sync');
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     *
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        $schedule->command(static::class)->hourlyAt(50);
+    }
+}
