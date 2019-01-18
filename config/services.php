6b32573dacac97d4929282414a241840f576fe6b
kawax <kawaxbiz@gmail.com>
2019-01-18 11:04:12 +0900

    discord

--- config/services.php
+++ config/services.php
@@ -15,7 +15,7 @@ return [
     */
 
     'discord' => [
-        'url'       => 'https://discord.gg/XukjEsV',
+        'url'       => 'https://discord.gg/BcChcXD',
         'prefix'    => '/',
         'not_found' => 'Command Not Found!',
         'path'      => [
