b911f5ca0cbdc89f4c518b70ddcb5d37c791f088
kawax <kawaxbiz@gmail.com>
2019-02-07 11:53:16 +0900

    package

---
+++ app/Commands/Packagist/PackageCommand.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Commands\Packagist;
+
+use LaravelZero\Framework\Commands\Command;
+
+use Closure;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Collection;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
+use Psr\Http\Message\ResponseInterface;
+
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\HashErrorNotification;
+
+use Symfony\Component\Console\Helper\ProgressBar;
+
+class PackageCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'packagist:package {provider}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Download package files';
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->client = resolve(Client::class);
+
+        $this->path = config('packagist.path');
+
+        $this->package($this->argument('provider'));
+    }
+
+    /**
+     * @param string $provider
+     */
+    protected function package(string $provider)
+    {
+        $urls = $this->packageUrls($provider);
+
+        $bar = $this->output->createProgressBar($urls->count());
+        $bar->setFormat(' %current%/%max% [%bar%] %percent:3s%% %message%');
+        $bar->setMessage('');
+        $bar->start();
+
+        $requests = function ($urls) {
+            foreach ($urls as $url) {
+                yield function () use ($url) {
+                    return $this->client->getAsync($url['url']);
+                };
+            }
+        };
+
+        $pool = new Pool($this->client, $requests($urls), [
+            'concurrency' => config('packagist.concurrency'),
+            'fulfilled'   => $this->packageFulfilled($urls, $bar),
+            'rejected'    => function ($reason, $index) use ($urls, $bar) {
+                $this->error('Package rejected: ' . $urls[$index]['package']);
+                $bar->advance();
+            },
+        ]);
+
+        $promise = $pool->promise();
+        $promise->wait();
+
+        $bar->finish();
+        $this->line('');
+    }
+
+    /**
+     * @param string $provider
+     *
+     * @return Collection
+     */
+    protected function packageUrls(string $provider): Collection
+    {
+        $packages = json_decode(Storage::get($this->path . $provider));
+
+        $packages = data_get($packages, 'providers');
+
+        return collect($packages)
+            ->unless(app()->environment('production'), function (Collection $collection) {
+                return $collection->take(10);
+            })->reject(function ($meta, $package) {
+                return Storage::exists($this->path . $this->packageFile($package, $meta));
+            })->map(function ($meta, $package) {
+                return [
+                    'package' => $package,
+                    'url'     => $this->packageFile($package, $meta),
+                    'sha'     => data_get($meta, 'sha256'),
+                ];
+            })->values();
+    }
+
+    /**
+     * @param Collection  $urls
+     * @param ProgressBar $bar
+     *
+     * @return Closure
+     */
+    protected function packageFulfilled(Collection $urls, ProgressBar $bar): Closure
+    {
+        return function (ResponseInterface $res, $index) use ($urls, $bar) {
+            $package = $urls[$index]['package'];
+
+            $content = $res->getBody()->getContents();
+
+            if (hash_equals(hash('sha256', $content), $urls[$index]['sha'])) {
+                Storage::put($this->path . $urls[$index]['url'], $content);
+            } else {
+                $this->hashError($package, $urls[$index]['url']);
+            }
+
+            $this->packageDelete($urls[$index]);
+
+            $bar->advance();
+            $bar->setMessage($package);
+        };
+    }
+
+    /**
+     * @param string $package
+     * @param object $meta
+     *
+     * @return string
+     */
+    protected function packageFile(string $package, $meta): string
+    {
+        return sprintf('p/%s$%s.json', $package, data_get($meta, 'sha256'));
+    }
+
+    /**
+     * @param array $url
+     */
+    protected function packageDelete(array $url)
+    {
+        $pattern = Storage::path($this->path . 'p/' . $url['package']) . '$*';
+
+        foreach (File::glob($pattern) as $file) {
+            if ($file !== Storage::path($this->path . $url['url'])) {
+                File::delete($file);
+            }
+        }
+    }
+
+    /**
+     * @param string $title
+     * @param string $url
+     */
+    protected function hashError(string $title, string $url)
+    {
+        $this->error('Hash error: ' . $url);
+
+        Notification::route('discord', config('services.discord.channel'))
+                    ->notify(new HashErrorNotification($title, $url));
+    }
+}
