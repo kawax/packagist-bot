80c1f68068920c9da85e2567cf9d48c09e6dde21
kawax <kawaxbiz@gmail.com>
2020-03-20 20:43:11 +0900

    Update schedule

--- app/Commands/Packagist/ReloadCommand.php
+++ app/Commands/Packagist/ReloadCommand.php
@@ -97,6 +97,7 @@ class ReloadCommand extends Command
     public function schedule(Schedule $schedule): void
     {
         $schedule->command(static::class, ['--quiet'])
-                 ->everyTenMinutes();
+                 ->everyTenMinutes()
+                 ->withoutOverlapping(60);
     }
 }
